b2d7261b391f0257c62ececbda7cce860a621c3e
Admin <taindFX13096@funix.edu.vn>
2022-02-11 15:14:18 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import { Navbar, NavbarBrand } from 'reactstrap';
+// import StaffList from './components/StaffListComponent.js';
+import StaffList from './components/StaffListComponent';
+import { STAFFS } from './shared/staffs';
+import { Component } from 'react/cjs/react.production.min';
+// import {STAFFS} from './shared/staffs.js';
 
-function App() {
+class App extends Component {
+// function App(props) {
+// function App() {
+  constructor (props) {
+    super(props);
+    this.state = {
+      // staffs : null,
+      staffs : STAFFS
+    };
+  }
+  render () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Ứng dụng quản lý nhân sự v1.0</h1>
+        {/* <h1 style={{color: "white", backgroundColor: "blue"}}>Ứng dụng quản lý nhân sự v1.0</h1> */}
+      </div>
+
+      {/* Add Navbar with reactstrap */}
+        {/* <Navbar>
+          <div>
+            <NavbarBrand>Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+          </div>
+        </Navbar> */}
+      {/* <StaffList staffs={this.state.staffs.name}/></StaffList> */}
+      {/* <StaffList name="Quần jean" type="Skinny" color ="Đen" size = "L">Clothes 1</StaffList> */}
+
+      <StaffList staffs={this.state.staffs}/>
     </div>
   );
+  }
 }
+// }
 
 export default App;
---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import '../App.css';
+// import './App.css';
+// import { STAFFS } from './shared/staffs';
+
+class StaffList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            staffs : null,
+            // staffs : STAFFS
+        };
+    }
+
+    onStaffClick(staff) {
+        this.setState({
+            staffs: staff,
+        });
+    }
+
+    renderstaff(staff) {
+        if(staff != null) {
+            return (
+                <div>
+                    <h4>Họ và tên: {staff.name}</h4>
+                    <p>Ngày sinh: {staff.doB}</p>
+                    <p>Ngày vào công ty: {staff.startDate}</p>
+                    <p>Phòng ban: {staff.department.name}</p>
+                    <p>Số ngày nghỉ còn lại: {staff.annualLeave}</p>
+                    <p>Số ngày đã làm thêm: {staff.overTime}</p>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <p>Bấm vào tên nhân viên để xem thông tin.</p>
+                </div>
+            );
+        }
+    }
+
+    render() {
+        
+            //const StaffList = (props) => {
+            // console.log(this.props) //Giá trị của props
+            // console.log(this.props.staffs[0].name);
+            const staffList = this.props.staffs.map((staff) => {
+                return (
+                    // <div style={{color: "red"}} onClick = {() => this.onStaffClick(staff)}>
+                    <div className="col-md-4" onClick = {() => this.onStaffClick(staff)}>
+                        {/* <li class="list">{staff.name}</li> */}
+                        <p class="list">{staff.name}</p>
+                    </div>
+                );
+            })
+            return (
+                <div className = "container">
+                    <div class = "row">
+                        {staffList}
+                    </div>
+                    <div>
+                        {/* {this.onStaffClick(this.state.staffs)} */}
+                        {this.renderstaff(this.state.staffs)}
+                    </div>
+                </div>
+            );
+
+    }
+
+}
+
+export default StaffList;
